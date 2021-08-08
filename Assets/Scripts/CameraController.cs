d22754980df448d53fb0643b8205164a555f9e56
Oleg <55650061+Oleg-Sergeev@users.noreply.github.com>
2021-08-08 16:53:34 +0300

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+
+    [SerializeField]
+    private Vector3 _offset = new Vector3(-5f, -9.3f, 5f);
+
+    [SerializeField]
+    private float _followSpeed;
+
+
+    private void Start()
+    {
+        if (_offset == Vector3.zero) _offset = _target.position - transform.position;
+    }
+
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, _target.position - _offset, _followSpeed * Time.deltaTime);
+    }
+}
